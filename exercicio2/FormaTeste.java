0d7bddff2ffbe484a4b978e332f0ffceb0e74f49
Pedro-Leles <pedro.alvim@soulasalle.com.br>
2025-06-05 18:38:18 -0300

    Create FormaTeste.java

---
+++ exercicio2/FormaTeste.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FormaTeste {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Retângulo
+        System.out.print("Digite a largura do retângulo: ");
+        double largura = scanner.nextDouble();
+        System.out.print("Digite a altura do retângulo: ");
+        double altura = scanner.nextDouble();
+        Retangulo retangulo = new Retangulo(largura, altura);
+        System.out.println("Área do Retângulo: " + retangulo.calcularArea());
+        System.out.println("Perímetro do Retângulo: " + retangulo.calcularPerimetro());
+
+        // Círculo
+        System.out.print("\nDigite o raio do círculo: ");
+        double raio = scanner.nextDouble();
+        Circulo circulo = new Circulo(raio);
+        System.out.println("Área do Círculo: " + circulo.calcularArea());
+        System.out.println("Perímetro do Círculo: " + circulo.calcularPerimetro());
+
+        scanner.close();
+    }
+}
